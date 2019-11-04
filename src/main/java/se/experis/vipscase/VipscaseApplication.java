945823c018c96c3bf13044a9371970f044645f5e
Per-Anders Karlsson <karlsson.peranders@gmail.com>
2019-11-04 13:22:31 +0100

    initial push

---
+++ src/main/java/se/experis/vipscase/VipscaseApplication.java
@@ -0,0 +1,13 @@
+package se.experis.vipscase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipscaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipscaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/experis/vipscase/VipscaseApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis.vipscase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipscaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
