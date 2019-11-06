8be13191f5cb6e02fc54b8331f46537b2f9eb225
Simon Nilsson <simon.nilsson123@gmail.com>
2019-11-06 13:46:59 +0100

    Added register/user, and closeConnect in db
    Register a user works via postman. Both db-query-functions now closes the connection after they're done.

--- src/main/java/se/experis/vipscase/controllers/UserOrderController.java
+++ src/main/java/se/experis/vipscase/controllers/UserOrderController.java
@@ -2,12 +2,20 @@ package se.experis.vipscase.controllers;
 
 
 import org.springframework.web.bind.annotation.*;
+import se.experis.vipscase.model.User;
+import se.experis.vipscase.Database;
 
+import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 @RestController
 public class UserOrderController {
-    UserOrderController(){}
+    UserOrderController(){
+
+    }
 
 
 
@@ -21,7 +29,14 @@ public class UserOrderController {
 
     @PostMapping("/register/user")
     @ResponseBody
-    public void registerUser(@RequestBody Object test) {
+    public void registerUser(@RequestBody User user) {
+        Database db = new Database();
+        Connection conn = db.connectToDb();
+        String sDate = user.getBirthdate();
+
+        db.insertQuery(conn, user.getName(), user.getPassword(), user.getEmail(), user.getLastname(), user.getStreet(), user.getPostcode(), user.getCity(), user.getBirthdate());
+        //TODO: Hash passwords
+
         //Simon
 
     }
---
+++ src/main/java/se/experis/vipscase/model/User.java
@@ -0,0 +1,99 @@
+package se.experis.vipscase.model;
+
+import java.util.Date;
+
+public class User {
+
+    User(){}
+
+    private int id;
+    private String name;
+
+    private String password;
+
+    private String email;
+
+    private String lastname;
+    private String street;
+    private int postcode;
+    private String city;
+    private String birthdate;
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+
+
+
+
+}
